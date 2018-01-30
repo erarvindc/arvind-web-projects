c5e877d7f0dd66e0cad8633d02a120147c5a16da
akku <akku@DESKTOP-1O2HP39>
2018-01-29 19:00:49 -0800

    Half cooked code with gmail signin

--- All_In_One/web/whats-cookin/js/main.js
+++ All_In_One/web/whats-cookin/js/main.js
@@ -8,14 +8,29 @@ var Main = {
 
 			$('#main-area').html(
 					
-					'<h3>This is the Main area for site content.</h3>'
+					'<h3>This is the Main area for site content.</h3>'// +
+					//'<div class="g-signin2" data-onsuccess="Main.onSignIn" data-theme="dark"></div>'
 			);
 			
-		}, 3000);
+		}, 1000 );
 		
 		
 		
-	}
+	},
+	onSignIn: function(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+    }
 		
 		
 }
